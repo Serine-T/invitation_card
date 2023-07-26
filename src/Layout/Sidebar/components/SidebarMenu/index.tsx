54aec5a77f7e3e20cff6a1abd3ebaf163d96aa92
Serine-t <serinetovmasyan98@gmail.com>
2023-07-26 11:25:56 +0400

    sidebar accardion extend

--- src/Layout/Sidebar/components/SidebarMenu/index.tsx
+++ src/Layout/Sidebar/components/SidebarMenu/index.tsx
@@ -1,15 +1,55 @@
-import { memo } from 'react';
+import React, { memo, useState } from 'react';
+
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { useLocation } from 'react-router-dom';
 
 import navData from './helpers';
-import SidebarAccardion from './SidebarAccardion';
+import { StyledAccordion, StyledAccordionSummary, StyledAccordionDetails } from './styled';
+import MenuItem from './MenuItem';
+import SubMenuItem from './SubMenu';
 
 const SidebarMenu = () => {
+  const { pathname } = useLocation();
+
+  const selectedPath = navData.find(({ path }) => pathname.startsWith(path));
+  const initialValue = selectedPath?.path || false;
+  const [expanded, setExpanded] = useState<string | false>(initialValue);
+
+  const handleChange =
+    (path: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+      setExpanded(isExpanded ? path : false);
+    };
+
   return (
     <>
       {
-        navData.map((item) => (
-          <SidebarAccardion key={item.path} {...item} />
-        ))
+        navData.map(({ path, title, children }) => {
+          const isActive = pathname.startsWith(path);
+
+          return (
+            <StyledAccordion key={path} expanded={expanded === path} onChange={handleChange(path)}>
+              <StyledAccordionSummary
+                isActive={isActive}
+                expandIcon={children && <ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id={path}
+              >
+                <MenuItem
+                  title={title}
+                  path={path}
+                  hasChild={!!children}
+                  isActive={isActive}
+                />
+              </StyledAccordionSummary>
+              {children &&
+        children.map((item) => (
+          <StyledAccordionDetails key={item.path}>
+            <SubMenuItem {...item} />
+          </StyledAccordionDetails>
+        ))}
+            </StyledAccordion>
+          );
+        })
       }
     </>
   );
