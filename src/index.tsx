c0c726886238aa526d706f8fc2bbc1ad6b10ce31
Serine-t <serinetovmasyan98@gmail.com>
2023-08-10 14:21:37 +0400

    change schema

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,3 @@
-// import React from 'react';
-
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import { store } from '@features/app/store';
