72d717801c9c189e60d6f54275fe8f9f774217d5
Serine-t <serinetovmasyan98@gmail.com>
2023-08-17 17:15:54 +0400

    add datepicker

--- src/containers/Orders/OpenJobs/index.tsx
+++ src/containers/Orders/OpenJobs/index.tsx
@@ -45,6 +45,18 @@ const OpenJobs = () => {
           tenetur aperiam sit! Dolores, praesentium commodi cupiditate iste optio exercitationem quod
           sed recusandae aspernatur nam quasi, a id deserunt ratione maiores repellendus
           quaerat quos tempora maxime minima enim ad animi ab accusamus! Incidunt, rerum?
+          sed recusandae aspernatur nam quasi, a id deserunt ratione maiores repellendus
+          quaerat quos tempora maxime minima enim ad animi ab accusamus! Incidunt, rerum?
+          What is your favorite day? Lorem ipsum dolor,
+          sit amet consectetur adipisicing elit. Debitis est quibusdam possimus animi sint ullam!
+          Hic quasi suscipit error. Soluta, aut magnam aspernatur deleniti, unde voluptates animi
+          placeat voluptatibus enim temporibus tempora et cumque qui. Exercitationem
+          sunt officiis cum reiciendis veniam minima impedit libero unde atque cupiditate
+          accusantium molestiae enim neque repellendus voluptatum modi quidem necessitatibus sint,
+          voluptatibus nemo sapiente blanditiis. Laboriosam, quo! Explicabo consectetur exercitationem
+          tenetur aperiam sit! Dolores, praesentium commodi cupiditate iste optio exercitationem quod
+          sed recusandae aspernatur nam quasi, a id deserunt ratione maiores repellendus
+          quaerat quos tempora maxime minima enim ad animi ab accusamus! Incidunt, rerum?
         </div>
       </FormProvider>
     </>
