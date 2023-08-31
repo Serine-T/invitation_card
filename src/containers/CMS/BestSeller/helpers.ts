b3f6bf49e9902af3c58c3be56ba0c8f7ff71a4c9
Serine-t <serinetovmasyan98@gmail.com>
2023-08-31 11:20:26 +0400

    remove ids

--- src/containers/CMS/BestSeller/helpers.ts
+++ src/containers/CMS/BestSeller/helpers.ts
@@ -1,14 +1,11 @@
 export const headSliderCells = [
   {
-    id: '1',
     label: 'Section',
   },
   {
-    id: '2',
     label: 'VISIBLE in section',
   },
   {
-    id: '4',
     label: 'ACTIONS',
     colSpan: 2,
   },
--- src/containers/Products/MenuCategories/helpers.ts
+++ src/containers/Products/MenuCategories/helpers.ts
@@ -1,14 +1,11 @@
 export const headSliderCells = [
   {
-    id: '1',
     label: 'Category',
   },
   {
-    id: '2',
     label: 'VISIBLE ON SITE',
   },
   {
-    id: '3',
     label: 'ACTIONS',
     colSpan: 2,
   },
