d11b3096406fa99e669940d93ee6762aa1f4f3aa
Serine-t <serinetovmasyan98@gmail.com>
2023-09-11 11:05:16 +0400

    remove cpmments

--- src/containers/common/FileUploader/requests.tsx
+++ src/containers/common/FileUploader/requests.tsx
@@ -19,9 +19,6 @@ export const uploadFile = async ({ file, url }: IUploadFileProps) => {
     withCredentials: false,
   };
 
-  // eslint-disable-next-line max-len
-  // const test = 'https://postcards-dev.s3.amazonaws.com/images/test-aaba6881-c768-4cbf-a234-44d1f46c1a25.jpeg?Content-Type=image%2Fjpeg&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA4552PZX4I2RYQH43%2F20230908%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230908T071543Z&X-Amz-Expires=900&X-Amz-Signature=4845b4c9a029eb45d1522cdb87ea24e7d1c081e5a12e75bfdc22d20964891213&X-Amz-SignedHeaders=host';
-
   const response = await axios.put<IUploadFileProps['file'], boolean>(url, file, options);
 
   return response;
